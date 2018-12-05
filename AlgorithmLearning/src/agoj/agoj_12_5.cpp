049e00b219f332065144e75c05532514a71cb5c0
SailHe <396103442@qq.com>
2018-12-05 18:50:39 +0800

    整理

--- AlgorithmLearning/src/agoj/agoj_12_5.cpp
+++ AlgorithmLearning/src/agoj/agoj_12_5.cpp
@@ -48,56 +48,86 @@ T maximumSubsectionSum(Iter left, Iter right, T MIN_VALUE) {
 	return maxSum;
 }
 
+/*
+约定: 
+before-now(时间上的先后);
+previous-current(位置上的先后);
+older-newer(内容,数据上的先后);
+
+start(ed)-stop-finish(ed)(开始, 停止中止, 完成: 时间上的起始);
+origin-target(动身,目标: 空间位置上的起始);
+begin-end(首先, 尽头: 内容上的起始);
+left-right(左, 右: 迭代器的起始 并不一定在位置上分别位于左和右;  也可以指下标; 或是一个左闭右开的范围保证!=号 left可以通过++使得left==right);
+from-to(来自, 到: 下标位置的起始 from <= to 或是用于 已有left和right的地方);
+lhs-rhs(左手边, 右手边: 泛指具有两个操作数的方法的左侧和右侧的两个操作数);
+
+ultimately: 最后, 根本
+final: 最终的, 决定性的
+sub: 替补者, 下标
+
+local: 局部的
+global: 全局的
+
+part: 部分(的)
+whole: 整体(的)
+
+result: 结果
+answer: 回答
+*/
+
 //局部和
 struct SubSum{
 	// 值
 	int value;
-	//起始位置 start-stop; begin-end; origin-target
-	int startSub;
+	//起始位置
+	int fromSub;
 	//终止位置
-	int stopSub;
+	int toSub;
 };
 void maximumSubsectionSum(ArrayList<int> const &arrSeq, int &maxSumOut, int &fromOut, int &toOut){
 	int ca = 1, t, n;
-	//读入目前数据前 总数据的最大和 before-now; previous-current; older-newer
-	SubSum before;
+	//读入目前数据前 总数据的最大和
+	SubSum previousMaxSubSum;
 	//读入数据的最大和
-	SubSum now;
+	SubSum currentMaxSubSum;
 	//整体最大和
-	SubSum max;
+	SubSum resultMaxSubSum;
 	auto it = arrSeq.begin();
 	if (it == arrSeq.end()) {
 		return;
 	}
 	n = (int)arrSeq.size();
-	now.value = *it;
+	currentMaxSubSum.value = *it;
 	++it;
-	for (now.startSub = now.stopSub = 1, max = before = now; now.stopSub < n; ){
-		now.value = *it;
+	for (currentMaxSubSum.fromSub = currentMaxSubSum.toSub = 1, resultMaxSubSum = previousMaxSubSum = currentMaxSubSum; currentMaxSubSum.toSub < n; ){
+		currentMaxSubSum.value = *it;
 		++it;
 
-		//now的起始位置是一样的
-		now.startSub = ++now.stopSub;
+		//currentMaxSubSum的起始位置是一样的
+		currentMaxSubSum.fromSub = ++currentMaxSubSum.toSub;
 		//更新局部最优解
-		if (now.value + before.value < now.value) {
+		// currentMaxSubSum.value + previousMaxSubSum.value < currentMaxSubSum.value <==> previousMaxSubSum.value < 0
+		// 前者可以理解为 横跨 之前的子列 和 当前的子列的子列 无法使比当前的子列更优
+		// 但这tm不是徒增脑活动量嘛
+		if (previousMaxSubSum.value < 0) {
 			//当前数据局部最优
-			before = now;
+			previousMaxSubSum = currentMaxSubSum;
 		}
 		else {
 			//之前数据局部最优
-			before.value += now.value;
+			previousMaxSubSum.value += currentMaxSubSum.value;
 		}
 
-		if (max.value < before.value) {
+		if (resultMaxSubSum.value < previousMaxSubSum.value) {
 			//更新整体最优解
-			max = before;
-			//max的末 位置要变化
-			max.stopSub = now.stopSub;
+			resultMaxSubSum = previousMaxSubSum;
+			//resultMaxSubSum 的终止位置要变化
+			resultMaxSubSum.toSub = currentMaxSubSum.toSub;
 		}
 	}
-	maxSumOut = max.value;
-	fromOut = max.startSub;
-	toOut = max.stopSub;
+	maxSumOut = resultMaxSubSum.value;
+	fromOut = resultMaxSubSum.fromSub;
+	toOut = resultMaxSubSum.toSub;
 }
 
 //	字符统计问题一 O(N)
