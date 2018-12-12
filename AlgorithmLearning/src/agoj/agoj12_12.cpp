408c6ffbff5215b489020013a76ffde05dbd875b
SailHe <396103442@qq.com>
2018-12-12 16:23:15 +0800

    最长不下降子序列 路径输出

--- AlgorithmLearning/src/agoj/agoj12_12.cpp
+++ AlgorithmLearning/src/agoj/agoj12_12.cpp
@@ -53,7 +53,54 @@ int mainForSolveC() {
 	return 0;
 }
 
+// 传入前驱列表 迭代路径 visit(int index); 没有前驱的结点 前驱值置为负数
+template<typename Fun>
+void iteratePath(ArrayList<int> const &precursorList, Fun visit, int origin = 0) {
+	for (int i = origin; i >= 0; i = precursorList[i]) {
+		visit(i);
+	}
+}
+
+// 完全是C的加强版 并没有删除代码
+int mainForSolveD() {
+	int n, maxLen, resOrigin;
+	while (1 == scanf("%d", &n)) {
+		ArrayList<int> dataList(n);
+		ArrayList<int> lenList(n, 0);
+		ArrayList<int> precursorList(n, -1);
+		for (int i = 0; i < n; ++i) {
+			scanf("%d", &dataList[i]);
+		}
+		maxLen = lenList[n - 1] = 1;
+		resOrigin = n - 1;
+		for (int i = n - 2; i > -1; --i) {
+			// 选择右侧符合条件(<=)的最优局部长度值中最大的那个作为前驱结点
+			int maxLocalRhsLen = 0;
+			for (int j = i + 1; j < n; ++j) {
+				if (dataList[i] <= dataList[j] && maxLocalRhsLen < lenList[j]) {
+					maxLocalRhsLen = lenList[j];
+					precursorList[i] = j;
+				}
+			}
+			lenList[i] = maxLocalRhsLen + 1;
+			if (lenList[i] > maxLen) {
+				maxLen = lenList[i];
+				resOrigin = i;
+			}
+		}
+		printf("%d\n", maxLen);
+		int prCnt = -1;
+		iteratePath(precursorList, [&](int index) {
+			printf((++prCnt == 0 ? "%d" : " %d"), dataList[index]);
+		}, resOrigin);
+		printf("\n");
+	}
+	return 0;
+}
+
+
 int main() {
-	mainForSolveC();
+	//mainForSolveC();
+	mainForSolveD();
 	return 0;
 }
