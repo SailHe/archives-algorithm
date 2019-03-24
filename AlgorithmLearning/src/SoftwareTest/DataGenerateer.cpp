01217d58c30e9b02ae9b66a8feccfb7116249358
SailHe <shell.sailhe@gmail.com>
2019-03-24 09:25:48 +0800

    整理

--- AlgorithmLearning/src/SoftwareTest/DataGenerateer.cpp
+++ AlgorithmLearning/src/SoftwareTest/DataGenerateer.cpp
@@ -1,36 +1,7 @@
-#include "SoftwareTest.h"
-
-/*
-文件编程例子
-fprintf(fp, "%s\n", s);
-fprintf(fp, "%d\n", n);
-fgets(ans, 5, fp);
-///fprintf(fp, (i == 0 ? "%d %d\n" : "%d %d\n"), le, ri); fprintf(fp, "\n"); ///
-
-srand(time(0));
-//随机输入生成
-//标程的输入输出
-//调用标程的到输出
-fp = fopen("sample.in", "w");
-freopen("sample.in", "r", stdin);
-freopen("sample.out", "w", stdout);
-*/
-
-/*
-0.StandardPrgram: 得到标程->将其main函数改为solution供数据生成的main调用
-1.测试时注释掉带有三个反斜线(///)的语句, 所有输入数据写在input文件内
-2.在控制台得出预期结果后打开刚刚注释掉的内容, 当前是从input读入数据结果存入新建的文件夹的0~9.in(out)文件中, 最多支持10组数据
-3.另外, 在输出测试数据时, 凡是有输入的地方都需要手动fprintf到fp中, 无法局部的变量使用全局变量*/
-
 #include <windows.h> // 创建目录
 #include <clocale> // 指定编码(c语言库locale.h)
-using namespace SoftwareTest;
-# include "SampleProgram\agoj19_03_20.cpp"
-// 此处调用例程
-int solution(){
-	SampleProgram_03_20().run();
-	return 0;
-}
+#include "SoftwareTest.h"
+#include "SampleProgram\SampleProgram.h"
 
 // @see https://gist.github.com/1901/5684151
 // @see https://www.cnblogs.com/hnrainll/archive/2011/05/07/2039700.html
@@ -72,22 +43,22 @@ int generateInputData(std::string &sampleFolderName, std::string &caseFileName, 
 	// 个数为1005, int范围内 每个数字最大值 21370
 	int base = 0;
 	while (testCaseCnt-- > 0) {
-		base = Random(200);
-		int leftBound = base, rightLimit = base + Random(21370 - base);
+		base = SoftwareTest::Random(200);
+		int leftBound = base, rightLimit = base + SoftwareTest::Random(21370 - base);
 		printf("%d %d\n", leftBound, rightLimit);
-		int size = 10 + Random(995);
+		int size = 10 + SoftwareTest::Random(995);
 		bool printfEnd = false;
 		for (int i = 0; i < size; ++i) {
-			base = Random(leftBound);
+			base = SoftwareTest::Random(leftBound);
 			int currentValue = -99999;
 			// 2/size 的机率遇见中途的中止标志(size > 10) 当然 遇见后就不会再次出现
-			if (IsProbability(size - 2, size) || printfEnd) {
+			if (SoftwareTest::IsProbability(size - 2, size) || printfEnd) {
 				// 0.5的概率上限减小三倍(为了使数据更随机: 负数可能, 小于下限概率更大)
 				int limit = 0;
 				SoftwareTest::loopUntilNotEq([&]() {
-					return limit = rightLimit - (base * (IsProbability(1, 2) ? 3 : 1));
+					return limit = rightLimit - (base * (SoftwareTest::IsProbability(1, 2) ? 3 : 1));
 				}, 0);
-				currentValue = Random(limit);
+				currentValue = SoftwareTest::Random(limit);
 			}
 			else {
 				printfEnd = true;
@@ -99,8 +70,14 @@ int generateInputData(std::string &sampleFolderName, std::string &caseFileName, 
 	return 0;
 }
 
-// 通过[解题样例程序]从[单个输入文件]计算出对应的[单个输出文件] (用例数据目录, 用例文件名, 时限ms)
-int calcSingleOutputData(std::string &sampleFolderName, std::string &inputCaseFileName, std::string &outputCaseFileName, double timeLimit, SoftwareTest::SampleProgram &sampleSolution) {
+// 通过[解题程序]从[单个输入文件]计算出对应的[单个输出文件] (用例数据目录, 用例文件名, 时限ms)
+int calcSingleOutputData(
+	std::string &sampleFolderName, 
+	std::string &inputCaseFileName, 
+	std::string &outputCaseFileName, 
+	double timeLimit, 
+	SoftwareTest::SampleProgram &sampleSolution
+) {
 	std::wstring wtmp;
 	toWstringFromString(sampleFolderName, wtmp);
 	BOOL flag = CreateDirectory(wtmp.c_str(), NULL);
@@ -120,7 +97,7 @@ int calcSingleOutputData(std::string &sampleFolderName, std::string &inputCaseFi
 }
 
 // 随机生成Input Data 同时计算出 Output Data (没有输入数据 没有解题程序 适用于那些不好写出解题程序的题)
-int generateInOut(std::string &sampleFolderName, std::string &caseFileName, int testCaseCnt) {
+int generateInputOutputData(std::string &sampleFolderName, std::string &caseFileName, int testCaseCnt) {
 	std::wstring wtmp;
 	toWstringFromString(sampleFolderName, wtmp);
 	BOOL flag = CreateDirectory(wtmp.c_str(), NULL);
@@ -136,24 +113,24 @@ int generateInOut(std::string &sampleFolderName, std::string &caseFileName, int 
 	// 个数为1005, int范围内 每个数字最大值 21370
 	int base = 0;
 	while (testCaseCnt-- > 0) {
-		base = Random(200);
-		int leftBound = base, rightLimit = base + Random(21370 - base);
+		base = SoftwareTest::Random(200);
+		int leftBound = base, rightLimit = base + SoftwareTest::Random(21370 - base);
 		printf("%d %d\n", leftBound, rightLimit);
-		int size = 10 + Random(995);
+		int size = 10 + SoftwareTest::Random(995);
 		bool printfEnd = false;
 		std::vector<int> integetList;
 		integetList.clear();
 		for (int i = 0; i < size; ++i) {
-			base = Random(leftBound);
+			base = SoftwareTest::Random(leftBound);
 			int currentValue = -99999;
 			// 2/size 的机率遇见中途的中止标志(size > 10) 当然 遇见后就不会再次出现
-			if (IsProbability(size - 2, size) || printfEnd) {
+			if (SoftwareTest::IsProbability(size - 2, size) || printfEnd) {
 				// 0.5的概率上限减小三倍(为了使数据更随机: 负数可能, 小于下限概率更大)
 				int limit = 0;
 				SoftwareTest::loopUntilNotEq([&]() {
-					return limit = rightLimit - (base * (IsProbability(1, 2) ? 3 : 1));
+					return limit = rightLimit - (base * (SoftwareTest::IsProbability(1, 2) ? 3 : 1));
 				}, 0);
-				currentValue = Random(limit);
+				currentValue = SoftwareTest::Random(limit);
 				if (!printfEnd && StandardExtend::inRange(leftBound, currentValue, rightLimit + 1) && integetList.size() < 1001) {
 					integetList.push_back(currentValue);
 				}
@@ -176,43 +153,12 @@ int generateInOut(std::string &sampleFolderName, std::string &caseFileName, int 
 	return 0;
 }
 
-// 多输入文件多输出文件 (已有单输入文件, 已有解题程序 生成对应的多个输入输出文件 最多支持0~9共10组)
-int generateMultipleInOut(){
-	bool flag = CreateDirectory((L"0123"), NULL);//文件夹名
-	char inputFilePath[20] = "0123/0.in";///
-	char outputFilePath[20] = "0123/0.out";///
-	/**/
-	freopen("input", "r", stdin); //从input读取输入数据
-	bool reInput = true;//重复输入
-	int n, c;
-	while (~scanf("%d%d", &n, &c)){
-
-		clock_t Start = clock();///
-		freopen(outputFilePath, "w", stdout);///
-		fp = fopen(inputFilePath, "w");/// 将输入数据写入in文件
-		fprintf(fp, "%d %d\n", n, c); ///
-		/**/
-
-		solution();
-
-		if (!reInput)
-			break;
-
-		++inputFilePath[5]; /// 多个in文件
-		++outputFilePath[5]; /// 多个out文件
-		clock_t Stop = clock();
-		if ((double)(Stop - Start) > 1000){// (double)(Stop - Start) / CLK_TCK //秒
-			puts("超时1000ms");
-		}
-		/**/
-	}
-	fclose(fp); ///
-	fp = NULL;
-	return 0;
-}
+// generateMultipleInOutData
+// 多输入文件多输出文件 (已有单个输入文件, 已有样例程序 生成对应的多个输入输出文件 最多支持0~9共10组)
+// 由于历史久远且可能没啥用处 已经删除 主要思想是使用数字命名的用例文件名, 然后文件名自增, 然后重新打开该文件(或重定向), 接下来就得具体分析了
 
 int main() {
-	int testCaseCnt = 2+1;
+	int testCaseCnt = 1000;
 	std::string testCaseFolderName("0123"), fileName("0");
 	/*int num, cnt = 0;
 	while (std::cin >> num) {
@@ -225,9 +171,9 @@ int main() {
 	return 0;
 	SampleProgram_03_20().run();
 	return 0;*/
-	generateInOut(testCaseFolderName, fileName, testCaseCnt);
+	generateInputOutputData(testCaseFolderName, fileName, testCaseCnt);
 	// generateInputData(testCaseFolderName, fileName, testCaseCnt);
-	calcSingleOutputData(testCaseFolderName, fileName, fileName + "_SP", 1000*3, SampleProgram_03_20());
+	calcSingleOutputData(testCaseFolderName, fileName, fileName + "_SP", 1000*30, SampleProgram_03_20());
 	SoftwareTest::JudgeByCompare(
 		(testCaseFolderName + "/" + fileName + ".out").c_str(),
 		(testCaseFolderName + "/" + fileName + "_SP" + ".out").c_str(),
